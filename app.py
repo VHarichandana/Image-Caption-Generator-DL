18ef03cd170b4739a0e3ac763ae82b7e1e17195c
VHarichandana <harichandanavanamamaly@gmail.com>
2026-05-20 00:08:30 +0530

    fix: correct import path for BahdanauAttention

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pickle
 import numpy as np
 
-from caption_model import BahdanauAttention
+from src.models.caption_model import BahdanauAttention
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
